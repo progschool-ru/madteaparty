884b58537f9b1a76b0347485076a2a603177c1f8
serdgo <serdgo@Sergeis-MacBook-Pro.local>
2016-12-30 02:43:11 +0600

    Stage #2

--- K.cpp
+++ K.cpp
@@ -11,11 +11,22 @@
 
 
 Этап 2.
-Прислать до 22:00 среды 28 декабря.
+Выполнил участник M.
 
 Останавливаем игру, когда осталось неотрицательное число, но взять ничего нельзя.
 Показываем, кто ходит: первый или второй.
 Финальное поздравление, где написано кто выиграл: первый или второй. Выигрывает тот, кто последним ходил.
+
+
+Этап 3.
+Прислать до субботы 31 декабря 23:59
+
+- Игрок не может взять больше, чем лежит в куче.
+- В конфигурационном файле conf.txt задано сколько лежит в куче, min и max.
+- Программа читает эту конфигурацию перед запуском и играет.
+- Если конфигурация противоречива,
+    выводим на экран информацию о том, что не понравилось,
+    сразу после этого выходим
 
 
 Известные проблемы:
@@ -35,20 +46,44 @@ int main()
     int min = 2;
     int max = 4;
 
+    int player_number=1;
     int current = start;
-    while (current > 0)
-    {
-        printf("Available: %d\n", current);
-        int player_takes;
+     printf("Course %d player\n",player_number);
+    while (current>=min)
+    {   
+         int player_takes;
+        printf("Available: %d\n", current);        
         scanf("%d", &player_takes);
         if (player_takes < min || player_takes > max)
         {
             printf("Invalid input. You can take any number >= %d and <= %d. Try again (%d <= X <= %d)\n", min, max, min, max);
+             if (player_number==1)
+            {
+            printf("Course 1 player\n");
+            }
+            else 
+            {    
+                 printf("Course 2 player\n");
+            }            
         }
         else
-        {
+        {  
             current -= player_takes;
+            if(current>=player_takes)
+            {           
+                if (player_number==1)
+                {
+                   player_number=player_number+1;
+                }
+                else 
+                {
+                    player_number=1;
+                }
+                 printf("Course %d player\n",player_number);
+            }
+
         }
     }
-    printf("The end of the game. Left: %d\n", current);
+    printf("The end of the game. Left: %d\n ", current);
+    printf("%d player wins", player_number);
 }
